da3a3911e3460fef1bbe078cbac4a68fda795d91
engineer <seradmin@vektornsk.local>
2026-07-27 12:34:27 +0700

    ddd

---
+++ ix.DataSource/Repositories/PostgresRepository.cs
@@ -0,0 +1,17 @@
+﻿using ix.Domain.Abstractions;
+using ix.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ix.DataSource.Repositories;
+
+public class PostgresRepository : IRepository
+{
+    public Task<bool> AddAsync(string fileName, IEnumerable<TimeScale> timeScalse)
+    {
+        throw new NotImplementedException();
+    }
+}
